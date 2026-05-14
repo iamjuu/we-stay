db8baef098bfdb0a989df4298658af264c388d39
nithin-yz <nithinkyz@gmail.com>
2026-05-15 03:02:11 +0530

    removed the second section

--- app/page.tsx
+++ app/page.tsx
@@ -244,7 +244,7 @@ export default function Home() {
         <InteriorTourClient />
       </div>
       <div>
-        {/* <BuilderNetwork /> */}
+        <BuilderNetwork />
       </div>
       <div>
         <TestimonialCarousel />
